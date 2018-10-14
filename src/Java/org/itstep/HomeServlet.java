cdd8027f5b01698c94ac25022fd5507ec48809de
aleksey939 <Filtron1>
2018-10-14 12:52:58 +0300

    init

---
+++ src/Java/org/itstep/HomeServlet.java
@@ -0,0 +1,28 @@
+package org.itstep;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+
+        String a=req.getParameter("a");
+        String b=req.getParameter("b");
+        String op=req.getParameter("op");
+        System.out.println("a"+a+"b"+b+"operat"+op);
+        switch (op){
+            case "add": int aInt = Integer.parseInt(a);
+                int bInt = Integer.parseInt(b);
+                System.out.println(aInt+bInt);
+                writer.println(aInt + bInt);
+        }
+    }
+}
